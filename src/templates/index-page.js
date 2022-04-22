6203a04afd9787356ca872518fb21fdf81ac33e6
TonyKnibb-MakaraHealth <tony@makarahealth.com>
2022-04-22 14:37:25 +0100

    Stripped all the tags and Bulma styling

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
+import {Link, graphql} from "gatsby";
+import {getImage} from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import Features from "../components/Features";
@@ -10,68 +10,68 @@ import FullWidthImage from "../components/FullWidthImage";
 
 // eslint-disable-next-line
 export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => {
-  const heroImage = getImage(image) || image;
+                                      image,
+                                      title,
+                                      heading,
+                                      subheading,
+                                      mainpitch,
+                                      description,
+                                      intro,
+                                  }) => {
+    const heroImage = getImage(image) || image;
 
-  return (
-    <div>
-      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
-      <h1 className="">{mainpitch.title}</h1>
-      <h3 className="">{mainpitch.description}</h3>
-      <h3 className="">{heading}</h3>
-      <p>{description}</p>
-      <Features gridItems={intro.blurbs} />
-      <Link className="" to="/products">See all products</Link>
-      <h3 className="">Latest stories</h3>
-      <BlogRoll />
-      <Link className="" to="/blog">Read more</Link>
-    </div>
-  );
+    return (
+        <div>
+            <FullWidthImage img={heroImage} title={title} subheading={subheading}/>
+            <h1 className="">{mainpitch.title}</h1>
+            <h3 className="">{mainpitch.description}</h3>
+            <h3 className="">{heading}</h3>
+            <p>{description}</p>
+            <Features gridItems={intro.blurbs}/>
+            <Link className="" to="/products">See all products</Link>
+            <h3 className="">Latest stories</h3>
+            <BlogRoll/>
+            <Link className="" to="/blog">Read more</Link>
+        </div>
+    );
 };
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    title: PropTypes.string,
+    heading: PropTypes.string,
+    subheading: PropTypes.string,
+    mainpitch: PropTypes.object,
+    description: PropTypes.string,
+    intro: PropTypes.shape({
+        blurbs: PropTypes.array,
+    }),
 };
 
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+const IndexPage = ({data}) => {
+    const {frontmatter} = data.markdownRemark;
 
-  return (
-    <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <IndexPageTemplate
+                image={frontmatter.image}
+                title={frontmatter.title}
+                heading={frontmatter.heading}
+                subheading={frontmatter.subheading}
+                mainpitch={frontmatter.mainpitch}
+                description={frontmatter.description}
+                intro={frontmatter.intro}
+            />
+        </Layout>
+    );
 };
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.shape({
+            frontmatter: PropTypes.object,
+        }),
     }),
-  }),
 };
 
 export default IndexPage;
