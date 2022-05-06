5a84d41c64c452bf2ca8c781954608d6883fed73
TonyKnibb-MakaraHealth <tony@makarahealth.com>
2022-05-06 10:34:36 +0100

    Messing with stuff

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,12 +2,13 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
+import IndexPagePreview from './preview-templates/IndexPagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import ProductPagePreview from './preview-templates/ProductPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import WorkPostPreview from './preview-templates/WorkPostPreview'
 import CareersPostPreview from './preview-templates/CareersPostPreview'
 import TeamMemberPreview from './preview-templates/TeamMemberPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
@@ -16,5 +17,6 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('work', WorkPostPreview)
 CMS.registerPreviewTemplate('careers', CareersPostPreview)
 CMS.registerPreviewTemplate('team', TeamMemberPreview)
