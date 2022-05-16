0a534dc7376d08ec4709f1fa57071453f091c056
TonyKnibb-MakaraHealth <tony@makarahealth.com>
2022-05-16 06:37:13 +0100

    Messing with stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,26 @@ module.exports = {
     './src/components/**/*.{html,js,jsx}',
   ],
   theme: {
-    screens: {},
-    colors: {
-      white: '#fff',
-      black: '#000',
-      blue: '#1a1f4c',
-      lightblue: '#2bb4dc',
-      turquoise: '#26c6b4',
-      grey: {
-        100: '#f7fafc',
-        300: '#f3f3f3',
-        500: '#a7a7a8',
-        900: '#1a202c',
+    extend: {
+      screens: {},
+      colors: {
+        white: '#fff',
+        black: '#000',
+        blue: '#1a1f4c',
+        lightblue: '#2bb4dc',
+        turquoise: '#26c6b4',
+        grey: {
+          100: '#f7fafc',
+          300: '#f3f3f3',
+          500: '#a7a7a8',
+          900: '#1a202c',
+        }
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
       }
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {}
+    }
   },
   plugins: []
 }
